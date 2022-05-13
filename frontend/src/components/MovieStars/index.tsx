efb72a5e20fd2507ad2c40bff8ab22264ff96bcd
dev-cesar <cesar.barbosa@bandtec.com.br>
2022-05-13 13:18:23 -0300

    Using star and score from API

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -1,13 +1,54 @@
 import './styles.css';
 
-function MovieStars() {
+type StarProps = {
+    fill:number;
+}
+
+type Props = {
+    score: number;
+}
+
+
+// EX:
+// getFills(3.5) => [1, 1, 1, 0.5, 0]
+// getFills(4.1) => [1, 1, 1, 1, 0.5]
+function getFills(score: number) {
+
+    const fills = [0, 0, 0, 0, 0];
+  
+    const integerPart = Math.floor(score);
+  
+    for (let i = 0; i < integerPart; i++) {
+      fills[i] = 1;
+    }
+  
+    const diff = score - integerPart;
+    if (diff > 0) {
+      fills[integerPart] = 0.5;
+    }
+  
+    return fills;
+}
+
+function Star({ fill } : StarProps) {
+    if(fill === 0) {
+        return <i className="dsmovie-star bi bi-star"></i>
+    } else if (fill === 1) {
+        return <i className="dsmovie-star bi bi-star-fill"></i>
+    }
+    return <i className="dsmovie-star bi bi-star-half"></i>;
+}
+
+function MovieStars({ score }: Props) {
+    const fills = getFills(score);
+
     return (
         <div className="dsmovie-stars-container">
-            <i className="dsmovie-star bi bi-star-fill"></i>
-            <i className="dsmovie-star bi bi-star-fill"></i>
-            <i className="dsmovie-star bi bi-star-fill"></i>
-            <i className="dsmovie-star bi bi-star-half"></i>
-            <i className="dsmovie-star bi bi-star"></i>
+            <Star fill={fills[0]} />
+            <Star fill={fills[1]} />
+            <Star fill={fills[2]} />
+            <Star fill={fills[3]} />
+            <Star fill={fills[4]} />
         </div>
     );
 }

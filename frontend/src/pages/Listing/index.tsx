1893e1818ded73dfbc4595dec39b433c3c9f2725
dev-cesar <cesar.barbosa@bandtec.com.br>
2022-05-13 11:00:04 -0300

    Using useState and useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,14 +1,21 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
+import { MoviePage } from "../../types/movie";
 import { BASE_URL } from "../../utils/request";
 
 function Listing() {
 
-    axios.get(`${BASE_URL}/movies`)
-    .then(res => {
-        console.log(res.data)
-    })
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies`)
+        .then(res => {
+            const data = res.data as MoviePage;
+            setPageNumber(data.number)
+        })
+    }, [])
 
     return (
         <>
